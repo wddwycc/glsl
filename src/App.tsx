d13caa5fea91fc111b21aa7450ddf777a109f87e
duan <wddwyss@gmail.com>
2019-09-05 23:06:44 +0900

    Minimum setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import raw from 'raw.macro'
+import React, { useEffect } from 'react'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getRenderContext = () => {
+  const canvas = document.querySelector('canvas')
+  if (!canvas) return
+  const gl = canvas.getContext('webgl')
+  if (!gl) return
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+  gl.clearColor(0, 0, 0, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  return gl
 }
 
-export default App;
+const App: React.FC = () => {
+  useEffect(() => {
+    const gl = getRenderContext()
+    if (!gl) return
+
+    const vertShader = gl.createShader(gl.VERTEX_SHADER)
+    if (!vertShader) return
+
+    const fragShader = gl.createShader(gl.FRAGMENT_SHADER)
+    if (!fragShader) return
+
+    gl.shaderSource(vertShader, raw('./shader.vert'))
+    gl.compileShader(vertShader)
+
+    gl.shaderSource(fragShader, raw('./shader.frag'))
+    gl.compileShader(fragShader)
+
+    const program = gl.createProgram()
+    if (!program) return
+
+    gl.attachShader(program, vertShader)
+    gl.attachShader(program, fragShader)
+    gl.linkProgram(program)
+    gl.detachShader(program, vertShader)
+    gl.detachShader(program, fragShader)
+    gl.deleteShader(vertShader)
+    gl.deleteShader(fragShader)
+
+    gl.useProgram(program)
+    gl.drawArrays(gl.POINTS, 0, 1)
+  }, [])
+
+  return (
+    <div id="container">
+      <canvas width={500} height={500} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/shader.frag
@@ -0,0 +1,5 @@
+#version 100
+
+void main() {
+  gl_FragColor = vec4(0.18, 0.54, 0.31, 1);
+}
